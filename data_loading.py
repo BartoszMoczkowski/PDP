fddf1f54d8962cfe280815d7083185e468710182
Bartosz Moczkowski <bartek.moczkowski@gmail.com>
2024-12-19 10:03:56 +0100

    started work on loading changfes

--- data_loading.py
+++ data_loading.py
@@ -7,6 +7,7 @@ from collections.abc import Callable
 
 class DataCore():
 
+
     """Main class for interacting with the data stored in the excel files
 
 
@@ -24,24 +25,30 @@ class DataCore():
 
         Failes if there is no excel file at the given path or
         if openpyxl is not installed"""
-        #TODO switch the columns and change the excel file templete
         #TODO remember that openpyxl must be installed for this to run
         #TODO verify loaded files
-        self.excel_data = pd.read_excel(path,engine="openpyxl")
+        excel_file = pd.ExcelFile(path)
+        self.excel_data = {}
+        for sheet_name in excel_file.sheet_names:
+            #probably better to add regex search for wavelengt in sheet name
+            self.excel_data[float(sheet_name)] = excel_file.parse(sheet_name,header=None)
 
     def create_excel_template(self,n_connectors,path="template.xlsx",n_wavelengths=1,n_fibers=1):
         
-        Sheet = np.zeros((n_connectors*n_fibers+2,n_connectors+2),dtype=object)
+
+        #
+        Sheet = np.zeros((n_connectors*n_connectors+2,n_fibers+2),dtype=object)
         
         #setting constant cells
         Sheet[0,0] = "Reference Configuration"
-        Sheet[0,1] = "Reference Connector"
-        Sheet[1,0] = "Fiber Number"
+        Sheet[1,0] = "Reference Connector"
+        Sheet[0,1] = ""
         Sheet[1,1] = "DUT"
         #setting numbering
-        Sheet[1,2:n_connectors+2] = np.linspace(1,n_connectors,n_connectors)
-        Sheet[2:n_fibers*n_connectors+2,1] = np.tile(np.linspace(1,n_connectors,n_connectors),n_fibers)
-        Sheet[2:n_fibers*n_connectors+2,0] = np.repeat(np.linspace(1,n_fibers,n_fibers),n_connectors)
+        Sheet[1,2:n_fibers+2] = np.linspace(1,n_fibers,n_fibers)
+        Sheet[2:n_connectors*n_connectors+2,1] = np.tile(np.linspace(1,n_connectors,n_connectors),n_connectors)
+        Sheet[2:n_connectors*n_connectors+2,0] = np.repeat(np.linspace(1,n_connectors,n_connectors),n_connectors)
+
 
         excel_df = pd.DataFrame(Sheet)
 
@@ -59,55 +66,65 @@ class DataCore():
             }
         )
         for worksheet in workbook.worksheets():
-            column_letter = xlsxwriter.utility.xl_col_to_name(n_connectors+1)
+            column_letter = xlsxwriter.utility.xl_col_to_name(n_fibers+2)
 
+            print(f"C1:{column_letter}1")
             worksheet.set_column("A:B",20)
-            worksheet.merge_range(f"C1:{column_letter}1", "Connector Number",merge_format)
-            for i in range(n_fibers):
+            worksheet.merge_range(f"C1:{column_letter}1", "Fiber Number",merge_format)
+            for i in range(n_connectors):
                 worksheet.merge_range(f"A{3+i*n_connectors}:A{2+(i+1)*n_connectors}", f"{i+1}",merge_format)
 
         writer.close()
 
 
-    def all_cells(self) -> np.ndarray:
-        """Returns values all cells read from the excel file"""
-        return self.excel_data.to_numpy()
+    def all_cells(self,data : pd.DataFrame) -> np.ndarray:
+        """Returns values all cells read from a given data fram
+        
+        Since data from the excel file is stored in a dictionary this should
+        be called on output of DataCore.IL_wavelength"""
+        return data.to_numpy()
 
 
-    def all_IL_values(self) -> np.ndarray:
+    def all_IL_values(self,data : pd.DataFrame) -> np.ndarray:
         """"Returns values of all cells where we expect IL values"""
         # bounds [3:,1;] correspond to the main portion of the data in the excel file
         # everything else is data about wavelengths and jumper/connectors indecies
-        return self.all_cells()[3:,1:]
+        return self.all_cells(data)[2:,2:]
 
-    def n_connectors(self) -> int:
+    def n_connectors(self,data : pd.DataFrame) -> int:
         """Returns the expected number of connectors present in the data"""
         #the second dimensions corresponds to the number of connectors
         # the first one cannot be used since it contains multiple entries
         # for different wavelengths
-        return self.all_IL_values().shape[1]
+        return int(np.sqrt(self.all_IL_values(data).shape[0]))
+    
+    def n_fibers(self,data : pd.DataFrame) -> int:
+        """Returns the expected number of fiber present in the data"""
+        #the second dimensions corresponds to the number of connectors
+        # the first one cannot be used since it contains multiple entries
+        # for different wavelengths
+        return self.all_IL_values(data).shape[1]
 
-    def n_fibers(self) -> int:
+    def n_jumpers(self,data : pd.DataFrame) -> int:
         """Returns the expected number of fibers present in the data"""
 
-        # there are two connectors per fiber
-        return self.n_connectors()//2
+        # there are two connectors per jumper
+        return self.n_connectors(data)//2
 
 
     def wavelengths(self) -> list[float]:
         """Returns a list of wavelenghs found in the data"""
-        wavelength_column = self.all_cells()[3:,0]
 
-        return list(filter(lambda x : not np.isnan(x),set(wavelength_column)))
+        return list(self.excel_data.keys())
 
 
-    def IL_connetor(self, index : int) -> np.ndarray:
+    def IL_reference_connetor(self, data : pd.DataFrame, index : int) -> np.ndarray:
         """Returns values of IL for a given connector (column)"""
 
-        return self.all_IL_values()[:,index]
+        return self.all_IL_values(data)[:,index*self.n_connectors():(index+1)*self.n_connectors()]
 
 
-    def IL_fiber(self, index : int) -> np.ndarray:
+    def IL_jumper(self, index : int) -> np.ndarray:
         """Returns values of IL for a given fiber (column)"""
         return self.all_IL_values()[:,[2*index, 2*index + 1]]
 
@@ -115,12 +132,11 @@ class DataCore():
     def IL_wavelength(self,wavelength : float) -> np.ndarray:
         """Returns values of IL for a given wavelength"""
 
-        wavelength_mask = self.all_cells()[3:,0] == wavelength
 
-        return self.all_IL_values()[wavelength_mask]
+        return self.excel_data[wavelength]
 
 
-    def filter_n_fibers(self,IL_data : np.ndarray, fiber_indecies : list[int]) -> np.ndarray:
+    def filter_n_fibers(self,IL_data : pd.DataFrame, fiber_indecies : list[int]) -> np.ndarray:
         """Filters the data to only include the data for a given fiber indecies
 
         Works only on data of shape m x m. In practive this means using data from
@@ -138,15 +154,20 @@ class DataCore():
             connector_indecies.append(2*fiber_index)
             connector_indecies.append(2*fiber_index+1)
 
-        return IL_data[connector_indecies,:][:,connector_indecies]
+        excel_indecies = []
+        for index_reference in connector_indecies:
+            for index_dut in connector_indecies:
+                excel_indecies.append(index_reference*self.n_connectors(IL_data)+index_dut)
+
+        return self.all_IL_values(IL_data)[excel_indecies,:]
 
 
-    def fiber_combinations(self, IL_data : np.ndarray, n_choices : int) -> list[np.ndarray]:
+    def fiber_combinations(self, IL_data : pd.DataFrame, n_choices : int) -> list[np.ndarray]:
 
-        if n_choices > self.n_fibers():
+        if n_choices > self.n_fibers(IL_data):
             print(f"Cannot chose {n_choices} from {self.n_fibers}.")
 
-        all_combinations_tupples = it.combinations(range(0,self.n_fibers()),n_choices)
+        all_combinations_tupples = it.combinations(range(0,self.n_fibers(IL_data)),n_choices)
 
         IL_data_combinations = []
 
@@ -175,12 +196,12 @@ class DataCore():
         """Map a function over a dictionary
 
         For a dictionary {a : x, b : y, ...} and a function f
-        this function returns """
+        this function returns {a : f(x), b : f(y), ...}"""
         return dict(map(lambda x : (x,func(d[x])),d))
 
 
     def IL_wavelengths(self) -> dict[float,np.ndarray]:
-        return {wavelength : self.IL_wavelength(wavelength) for wavelength in self.wavelengths()}
+        return {wavelength : self.all_IL_values(self.IL_wavelength(wavelength)) for wavelength in self.wavelengths()}
 
 
     def IL_connectors(self) -> list[np.ndarray]:
@@ -201,9 +222,19 @@ if __name__ == "__main__":
 
     #example
     DC = DataCore()
-    DC.load_excel("RM example data.xlsx")
+    DC.load_excel("E60F4420.xlsx")
 
-    wave_combinations_IL_unfiltered = DC.fiber_combinations_all_wavelengths(10)
+
+    wavelength_ex = DC.wavelengths()[0]
+
+    test_sheet = DC.IL_wavelength(wavelength_ex)
+
+    print(f"Number of connectors {DC.n_connectors(test_sheet)}")
+    print(f"Number of jumper {DC.n_jumpers(test_sheet)}")
+    print(f"Number of fiber {DC.n_fibers(test_sheet)}")
+
+    wave_combinations_IL_unfiltered = DC.fiber_combinations_all_wavelengths(4)
+    print(wave_combinations_IL_unfiltered)
     wave_combinations_IL = DC.map_dict(DC.filter_nan, wave_combinations_IL_unfiltered)
 
     wave_combinations_IL_mean = DC.map_dict(lambda arr : np.mean(arr,axis=1), wave_combinations_IL)
